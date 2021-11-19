16684210f087d0302955c681ff7b7f9e0aff8124
Nikita Chagay <nachagay@student.rtc.edu>
2021-11-18 19:19:53 -0800

    Functional code

--- jobhunter.py
+++ jobhunter.py
@@ -10,7 +10,7 @@ import html2text
 # You may need to edit the connect function based on your local settings.#I made a password for my database because it is important to do so. Also make sure MySQL server is running or it will not connect
 def connect_to_sql():
     conn = mysql.connector.connect(user='root', password='',
-                                   host='127.0.0.1', database='cne340')
+                                   host='127.0.0.1', database='job_hunter')
     return conn
 
 
@@ -35,8 +35,13 @@ def add_new_job(cursor, jobdetails):
     # extract all required columns
     description = html2text.html2text(jobdetails['description'])
     date = jobdetails['publication_date'][0:10]
-    query = cursor.execute("INSERT INTO jobs( Description, Created_at " ") "
-               "VALUES(%s,%s)", (  description, date))
+    Job_id = jobdetails['id']
+    url = jobdetails['url']
+    Title = jobdetails['title']
+    company = jobdetails['company_name']
+
+    query = cursor.execute("INSERT INTO jobs( Description, Created_at " ", Job_id, url, Title, company) "
+               "VALUES(%s,%s,%s,%s,%s,%s)", (  description, date, Job_id, url, Title, company))
      # %s is what is needed for Mysqlconnector as SQLite3 uses ? the Mysqlconnector uses %s
     return query_sql(cursor, query)
 
@@ -44,13 +49,15 @@ def add_new_job(cursor, jobdetails):
 # Check if new job
 def check_if_job_exists(cursor, jobdetails):
     ##Add your code here
-    query = "UPDATE"
+    id = jobdetails['id']
+    query = "SELECT * FROM jobs WHERE Job_id = \"%s\"" % id
     return query_sql(cursor, query)
 
 # Deletes job
 def delete_job(cursor, jobdetails):
     ##Add your code here
-    query = "UPDATE"
+    id = jobdetails['id']
+    query = "DELETE * FROM jobs WHERE Job_id = \"%s\"" % id
     return query_sql(cursor, query)
 
 
@@ -78,9 +85,14 @@ def add_or_delete_job(jobpage, cursor):
         check_if_job_exists(cursor, jobdetails)
         is_job_found = len(
         cursor.fetchall()) > 0  # https://stackoverflow.com/questions/2511679/python-number-of-rows-affected-by-cursor-executeselect
+
         if is_job_found:
+            print("job is found: "+ jobdetails["title"] + " from " + jobdetails["company_name"])
 
         else:
+            print("New job is found: "+ jobdetails["title"] + " from " + jobdetails["company_name"])
+            add_new_job(cursor, jobdetails)
+
             # INSERT JOB
             # Add in your code here to notify the user of a new posting. This code will notify the new user
 
@@ -95,9 +107,9 @@ def main():
     cursor = conn.cursor()
     create_tables(cursor)
 
-    while (1):  # Infinite Loops. Only way to kill it is to crash or manually crash it. We did this as a background process/passive scraper
+    while 1:  # Infinite Loops. Only way to kill it is to crash or manually crash it. We did this as a background process/passive scraper
         jobhunt(cursor)
-        time.sleep(21600)  # Sleep for 1h, this is ran every hour because API or web interfaces have request limits. Your reqest will get blocked.
+        time.sleep(21600)  # Sleep for 1h, this is ran every hour because API or web interfaces have request limits. Your request will get blocked.
 
 
 # Sleep does a rough cycle count, system is not entirely accurate
